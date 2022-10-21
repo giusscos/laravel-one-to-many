9946f8b85fecb59de6192c83d0a6f50489651a68
giusscos <giu.cosenza08@gmail.com>
2022-10-21 15:30:54 +0200

    Add model, controller, migration, seeder category

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -11,40 +11,42 @@
             </a>
         </div>
     </div>
-    <div class="table-responsive">
-        <table class="table table-striped table-hover">
-            <thead class="thead-dark">
-                <tr>
-                    <th>#</th>
-                    <th>Titolo</th>
-                    <th>Slug</th>
-                    <th>Creato il</th>
-                    <th>Aggiornato il</th>
-                    <th></th>
-                    <th></th>
-                    <th></th>
-                </tr>
-            </thead>
-            @foreach ($posts as $post)
-                <tr>
-                    <td> {{ $post->id }} </td>
-                    <td> {{ $post->title }} </td>
-                    <td> {{ $post->slug }} </td>
-                    <td> {{ $post->created_at }} </td>
-                    <td> {{ $post->updated_at }} </td>
-                    <td><a href="{{ route('admin.posts.show', $post) }}" class="btn btn-primary">Scopri</a></td>
-                    <td><a href="{{ route('admin.posts.edit', $post) }}" class="btn btn-primary">Modifica</a></td>
-                    <td>
-                        <form action="{{ route('admin.posts.destroy', $post) }}" method="POST">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit" class="btn btn-danger">
-                                Elimina
-                            </button>
-                        </form>
-                    </td>
-                </tr>
-            @endforeach
-        </table>
+    <div class="container-lg">
+        <div class="table-responsive">
+            <table class="table table-striped table-hover">
+                <thead class="thead-dark">
+                    <tr>
+                        <th>#</th>
+                        <th>Titolo</th>
+                        <th>Slug</th>
+                        <th>Creato il</th>
+                        <th>Aggiornato il</th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                    </tr>
+                </thead>
+                @foreach ($posts as $post)
+                    <tr>
+                        <td> {{ $post->id }} </td>
+                        <td> {{ $post->title }} </td>
+                        <td> {{ $post->slug }} </td>
+                        <td> {{ $post->created_at }} </td>
+                        <td> {{ $post->updated_at }} </td>
+                        <td><a href="{{ route('admin.posts.show', $post) }}" class="btn btn-primary">Scopri</a></td>
+                        <td><a href="{{ route('admin.posts.edit', $post) }}" class="btn btn-primary">Modifica</a></td>
+                        <td>
+                            <form action="{{ route('admin.posts.destroy', $post) }}" method="POST">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="btn btn-danger">
+                                    Elimina
+                                </button>
+                            </form>
+                        </td>
+                    </tr>
+                @endforeach
+            </table>
+        </div>
     </div>
 @endsection
